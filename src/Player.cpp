34f1ecb0cd22de31ae0385ad80fcc686adfa6dc3
veawenn <andrea.velez@estudiantat.upc.edu>
2025-10-23 08:23:43 +0200

    Handout10

---
+++ src/Player.cpp
@@ -0,0 +1,152 @@
+#include "Player.h"
+#include "Engine.h"
+#include "Textures.h"
+#include "Audio.h"
+#include "Input.h"
+#include "Render.h"
+#include "Scene.h"
+#include "Log.h"
+#include "Physics.h"
+#include "EntityManager.h"
+#include "Map.h"
+
+Player::Player() : Entity(EntityType::PLAYER)
+{
+	name = "Player";
+}
+
+Player::~Player() {
+
+}
+
+bool Player::Awake() {
+
+	//L03: TODO 2: Initialize Player parameters
+	position = Vector2D(96, 96);
+	return true;
+}
+
+bool Player::Start() {
+
+	//L03: TODO 2: Initialize Player parameters
+	//L10: TODO 3; Load the spritesheet of the player
+	texture = Engine::GetInstance().textures->Load("Assets/Textures/player1.png");
+
+	//L10: TODO 3: Load the spritesheet animations from the TSX file
+
+	// L08 TODO 5: Add physics to the player - initialize physics body
+	texW = 32;
+	texH = 32;
+	pbody = Engine::GetInstance().physics->CreateCircle((int)position.getX(), (int)position.getY(), texW / 2, bodyType::DYNAMIC);
+
+	// L08 TODO 6: Assign player class (using "this") to the listener of the pbody. This makes the Physics module to call the OnCollision method
+	pbody->listener = this;
+
+	// L08 TODO 7: Assign collider type
+	pbody->ctype = ColliderType::PLAYER;
+
+	//initialize audio effect
+	pickCoinFxId = Engine::GetInstance().audio->LoadFx("Assets/Audio/Fx/coin-collision-sound-342335.wav");
+
+	return true;
+}
+
+bool Player::Update(float dt)
+{
+	Physics* physics = Engine::GetInstance().physics.get();
+
+	// Read current velocity
+	b2Vec2 velocity = physics->GetLinearVelocity(pbody);
+	velocity = { 0, velocity.y }; // Reset horizontal velocity
+
+	// Move left/right
+	if (Engine::GetInstance().input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) {
+		velocity.x = -speed;
+		//L10: TODO 6: Update the animation based on the player's state
+
+	}
+	if (Engine::GetInstance().input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) {
+		velocity.x = speed;
+		//L10: TODO 6: Update the animation based on the player's state
+
+	}
+
+	// Jump (impulse once)
+	if (Engine::GetInstance().input->GetKey(SDL_SCANCODE_SPACE) == KEY_DOWN && isJumping == false) {
+		physics->ApplyLinearImpulseToCenter(pbody, 0.0f, -jumpForce, true);
+		//L10: TODO 6: Update the animation based on the player's state
+		isJumping = true;
+	}
+
+	// Preserve vertical speed while jumping
+	if (isJumping == true) {
+		velocity.y = physics->GetYVelocity(pbody);
+	}
+
+	// Apply velocity via helper
+	physics->SetLinearVelocity(pbody, velocity);
+
+	// L10: TODO 5: Update the animation based on the player's state (moving, jumping, idle)
+
+	// Update render position using your PhysBody helper
+	int x, y;
+	pbody->GetPosition(x, y);
+	position.setX((float)x);
+	position.setY((float)y);
+
+	//L10: TODO 7: Center the camera on the player
+
+	// L10: TODO 5: Draw the player using the texture and the current animation frame
+	Engine::GetInstance().render->DrawTexture(texture, x - texW / 2, y - texH / 2);
+	return true;
+}
+
+bool Player::CleanUp()
+{
+	LOG("Cleanup player");
+	Engine::GetInstance().textures->UnLoad(texture);
+	return true;
+}
+
+// L08 TODO 6: Define OnCollision function for the player. 
+void Player::OnCollision(PhysBody* physA, PhysBody* physB) {
+	switch (physB->ctype)
+	{
+	case ColliderType::PLATFORM:
+		LOG("Collision PLATFORM");
+		//reset the jump flag when touching the ground
+		isJumping = false;
+		//L10: TODO 6: Update the animation based on the player's state
+
+		break;
+	case ColliderType::ITEM:
+		LOG("Collision ITEM");
+		Engine::GetInstance().audio->PlayFx(pickCoinFxId);
+		physB->listener->Destroy();
+		break;
+	case ColliderType::UNKNOWN:
+		LOG("Collision UNKNOWN");
+		break;
+	default:
+		break;
+	}
+}
+
+void Player::OnCollisionEnd(PhysBody* physA, PhysBody* physB)
+{
+	switch (physB->ctype)
+	{
+	case ColliderType::PLATFORM:
+		LOG("End Collision PLATFORM");
+		break;
+	case ColliderType::ITEM:
+		LOG("End Collision ITEM");
+		break;
+	case ColliderType::UNKNOWN:
+		LOG("End Collision UNKNOWN");
+		break;
+	default:
+		break;
+	}
+}
+
