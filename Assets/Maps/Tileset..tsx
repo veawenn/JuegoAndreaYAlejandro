88dbd8a2d86da265e6e45f39975fdac93a910c79
veawenn <andrea.velez@estudiantat.upc.edu>
2025-11-01 22:43:40 +0100

    Add files via upload

---
+++ Assets/Maps/Tileset..tsx
@@ -0,0 +1,458 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Tileset" tilewidth="32" tileheight="32" tilecount="144" columns="16">
+ <image source="Tileset.png" width="512" height="288"/>
+ <tile id="0">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup>
+   <object id="1" type="Lamparas suelo" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup>
+   <object id="1" type="Lamparas suelo" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup>
+   <object id="1" type="Lamparas suelo" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup>
+   <object id="1" type="Lamparas suelo" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="28">
+  <objectgroup>
+   <object id="1" type="Lamparas suelo" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="29">
+  <objectgroup>
+   <object id="1" type="Lamparas suelo" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="30">
+  <objectgroup>
+   <object id="1" type="Lamparas suelo" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="31">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Lamparas suelo" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="34">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Suelo sin hierva" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="39">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="40">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Suelo hierva/piedra" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="46">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="47">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Suelo hierva/piedra al reves" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="52">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="53">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="54">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="55">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="56">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="57">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="58">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="60">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="61">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="62">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="63">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Plataformas horizontales piedra" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="68">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="69">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="70">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="71">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="72">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="73">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="74">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+   <object id="2" type="Diferentes suelos" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="76">
+  <objectgroup>
+   <object id="1" type="Hierva patron 2" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="77">
+  <objectgroup>
+   <object id="1" type="Hierva patron 2" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="78">
+  <objectgroup>
+   <object id="1" type="Hierva patron 2" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="79">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Hierva patron 2" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="81">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="82">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="87">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Subida piedra" x="31.2727" y="0.363636">
+    <polygon points="0,0 -31.0909,31.2727 0.727273,31.8182"/>
+   </object>
+   <object id="2" type="Subida piedra" x="31.8182" y="0">
+    <polygon points="0,0 -31.2727,31.2727 0.181818,31.8182"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="88">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Bajada piedra" x="0.545455" y="0.363636">
+    <polygon points="0,0 -0.727273,31.6364 31.2727,31.6364"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="89">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Subida" x="31.4545" y="0.363636">
+    <polygon points="0,0 -31.2727,31.2727 0.545455,31.4545"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="90">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Bajada" x="0.363636" y="0.545455">
+    <polygon points="0,0 -0.181818,31.0909 31.6364,31.4545"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="93">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="94">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="95">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+   <object id="2" type="Plataformas horizontales" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="97">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="98">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="103">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="104">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Suelo subida/bajada piedra" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="105">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="106">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Suelo subida/bajada" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="109">
+  <objectgroup>
+   <object id="1" type="Suelo" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="110">
+  <objectgroup>
+   <object id="1" type="Suelo" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="111">
+  <objectgroup>
+   <object id="1" type="Suelo" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="113">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Plataformas verticales" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="114">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="116">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Subida light piedra" x="0" y="31.8182">
+    <polygon points="0,0 31.8182,-16.1818 32,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="117">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Subida light piedra" x="31.6364" y="0.363636">
+    <polygon points="0,0 -31.2727,15.0909 -31.4545,31.6364 0.363636,31.8182"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="118">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Bajada light piedra" x="-0.181818" y="0">
+    <polygon points="0,0 0,32.1818 31.8182,32 32,15.0909"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="119">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Bajada light piedra" x="0.181818" y="16.1818">
+    <polygon points="0,0 0,15.8182 31.4545,15.8182"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="120">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Subida light" x="0" y="31.6364">
+    <polygon points="0,0 31.4545,-15.8182 31.8182,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="121">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Subida light" x="0" y="15.2727">
+    <polygon points="0,0 0.181818,16.7273 31.8182,16.9091 31.8182,-15.0909"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="122">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Bajada light" x="0.181818" y="0">
+    <polygon points="0,0 0.181818,32 32,32.1818 31.8182,15.0909"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="123">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Bajada light" x="0.181818" y="15.8182">
+    <polygon points="0,0 -0.181818,16 31.8182,16.3636"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="125">
+  <objectgroup>
+   <object id="1" type="Suelo" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="126">
+  <objectgroup>
+   <object id="1" type="Suelo" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="127">
+  <objectgroup>
+   <object id="1" type="Suelo" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="132">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="133">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="134">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="135">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Suelo subida/bajada light piedra" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="136">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="137">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="138">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="139">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Suelo bajada light" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="141">
+  <objectgroup>
+   <object id="1" type="Suelo" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="142">
+  <objectgroup>
+   <object id="1" type="Suelo" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="143">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Suelo" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
