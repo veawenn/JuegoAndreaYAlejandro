cf1cd511ef34cbf4a07eda348adf12a286238fd9
veawenn <andrea.velez@estudiantat.upc.edu>
2025-11-01 22:44:50 +0100

    Update Scene.cpp

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -44,7 +44,7 @@ bool Scene::Start()
 	//Engine::GetInstance().audio->PlayMusic("Assets/Audio/Music/level-iv-339695.wav");
 
 	//L06 TODO 3: Call the function to load the map. 
-	Engine::GetInstance().map->Load("Assets/Maps/", "PlataformGame..tmx");
+	Engine::GetInstance().map->Load("Assets/Maps/", "PlataformGame.tmx");
 	
 	return true;
 }
